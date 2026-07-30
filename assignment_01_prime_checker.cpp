43d18c0b7c9f0c6bb8bf0e3e747e86146e39dce8
Richmond Azuntaaba <richyazuns@gmail.com>
2026-07-30 02:57:30 +0000

    Completed prime number checker

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,41 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int number)
+{
+    // Numbers less than 2 are not prime
+    if (number < 2)
+    {
+        return false;
+    }
+
+    // Check for divisors from 2 up to number-1
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number))
+    {
+        cout << number << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
